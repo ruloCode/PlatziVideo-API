77ccf943d577813c51cf82fff895d6f6b971f68b
Rulo_code <drew.lizcano@gmail.com>
2020-02-11 14:12:37 -0500

    mongo lib

--- config/index.js
+++ config/index.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const config = {
   dev: process.env.NODE_ENV != 'production',
   port: process.env.PORT || 3000,
+  PORT: process.env.PORT,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DR_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME
 }
 
 module.exports = {
