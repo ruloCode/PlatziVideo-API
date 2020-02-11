a9450b965db4b34ba815429fdac2d17c80c2184d
Rulo_code <drew.lizcano@gmail.com>
2020-02-11 10:01:44 -0500

    creando CRUD con mokcks

--- routes/movies.js
+++ routes/movies.js
@@ -6,6 +6,7 @@ function moviesApi(app) {
   const router = express.Router();
   app.use('/api/movies', router)
 
+  // GET ALL MOVIES
   router.get('/', async (req, res, next) => {
     try {
       const movies = await Promise.resolve(moviesMock)
@@ -17,6 +18,60 @@ function moviesApi(app) {
       next(err)
     }
   })
+
+  // GET ONE MOVIE
+  router.get('/:movieId', async (req, res, next) => {
+    try {
+      const movies = await Promise.resolve(moviesMock[0])
+      res.status(200).json({
+        data: movies,
+        message: 'movie retrieve'
+      })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  // CREATE ONE MOVIE
+  router.post('/', async (req, res, next) => {
+    try {
+      const createdMovieId = await Promise.resolve(moviesMock[0].id)
+      res.status(201).json({
+        data: createdMovieId,
+        message: 'movie created'
+      })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  // UPDATE ONE MOVIE
+  router.put('/:movieId', async (req, res, next) => {
+    try {
+      const updatedMovieId = await Promise.resolve(moviesMock[0].id)
+      res.status(200).json({
+        data: updatedMovieId,
+        message: 'movie updated'
+      })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  // DELETE ONE MOVIE
+  router.delete('/:movieId', async (req, res, next) => {
+    try {
+      const deletedMovieId = await Promise.resolve(moviesMock[0].id)
+      res.status(200).json({
+        data: deletedMovieId,
+        message: 'movie deleted'
+      })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+
 }
 
 module.exports = moviesApi;
