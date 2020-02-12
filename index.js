7e4b46b7f0d3f9a8da94b983b2314cd50dcac8d4
Rulo_code <drew.lizcano@gmail.com>
2020-02-11 19:43:36 -0500

    boom erros

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const app = express()
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers')
+const notFoundHandler = require('./utils/middleware/notFoundHandler')
 
 // body parser
 app.use(express.json());
 
-
+// routes
 moviesApi(app)
+
+//not found 404
+app.use(notFoundHandler)
 
 //error middlewares
 app.use(logErrors);
-app.use(errorHandler)
+app.use(wrapErrors);
+app.use(errorHandler);
+
+
 
 app.listen(config.port, ()=> {
   console.log(`Server running on http://localhost:${config.port}`)
